62711252c2816dd2919986f2a73b1442afdbf8be
Ryan Cleven <rcleven@mac.com>
2021-10-12 23:45:35 -0700

    Added simple vector array wrapper

---
+++ Vec2Array.hx
@@ -0,0 +1,31 @@
+
+
+#if hl
+abstract Vec2Array(hl.NativeArray<Single>)  from hl.NativeArray<Single> to hl.NativeArray<Single>  {
+
+
+    public function new( x : hl.NativeArray<Single> ) {
+        this = x;
+    }
+
+    public var length(get, never):Int;
+
+	extern inline function get_length():Int {
+		return this.length;
+	}
+
+    @:arrayAccess inline function getv(k:Int) : Vec2{
+        return new Vec2( this[k * 2], this[k * 2 + 1]);
+      }
+    
+      @:arrayAccess inline function setv(k:Int, v : Vec2) {
+        this[k*2] = v.x;
+        this[k*2+1] = v.y;
+      }
+    
+    public static function allocate(count): Vec2Array {
+        return new Vec2Array(new hl.NativeArray<Single>(count * 2));
+    }
+}
+
+#end
